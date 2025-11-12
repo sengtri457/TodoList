b6026d9a5ad0fb09a90b99177df17abdea801049
Sengtri Bun <sengtri457@gmail.com>
2025-11-12 22:42:02 +0700

    update vercel

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -13,6 +13,7 @@ const protect = async (req, res, next) => {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       req.user = await User.findById(decoded.id).select("-password");
+      console.log(decoded.id);
       next();
     } catch (err) {
       console.error(err);
